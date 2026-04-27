a4b4c31c6cd68d3aa7363aaf19ebec48a414f8ca
Tanushree B <tbhatta999@gmail.com>
2026-04-27 17:31:13 +0530

    Add Demo class with a zero argument constructor

---
+++ Argument.java
@@ -0,0 +1,9 @@
+class Demo {
+    Demo() {
+        System.out.println("Zero argument constructor called");
+    }
+
+    public static void main(String[] args) {
+        Demo obj = new Demo();
+    }
+}
